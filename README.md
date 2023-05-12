974a35c509cba0d04db30453172c503dc70ee175
Tom Cumming <mail@tomcumming.co.uk>
2023-05-12 21:16:56 +0100

    Remove home-manager

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
-## Rebuild Home Manager Config
+A nix setup without home-manager!
 
+## Setting up
+
+Install nix with DeterminateSystems/nix-installer.
+
+Symlink config:
 ```bash
-home-manager switch --flake './flake.nix#macmini'
+ln -s /path/to/here/.config ~/.config
+```
+
+Add to registry, start dev shell for the first time to set settings:
+```bash
+nix registry add config /path/to/here
+nix develop 'config#macmini'
 ```
--- flake.nix
+++ flake.nix
@@ -1,32 +1,64 @@
 {
-  description = "My home manager config";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     unixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, unixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, unixpkgs }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
       upkgs = unixpkgs.legacyPackages.${system};
+
+      zshrc = ''
+        if [ -z \"$IN_NIX_SHELL\" ]; then
+          # Not in dev shell
+        else
+          if [ -z \"$IN_OUTER_SHELL\" ]; then
+            export PROMPT=\'%F{red}%1~ %f$ \'
+          else
+            export PROMPT=\'%F{blue}%1~ %f$ \'
+          fi
+        fi
+
+        alias start-outer-shell="nix develop --offline \'config#macmini\' -c tmux"
+        alias nixdev="nix develop -u \'IN_OUTER_SHELL\' -c zsh"
+        
+        # Fix annoying osx keys
+        bindkey "^[[1;5C" forward-word
+        bindkey "^[[1;5D" backward-word
+      '';
     in
     {
       formatter.${system} = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
 
-      homeConfigurations.macmini = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      devShells.${system}.macmini = pkgs.mkShell {
+        name = "Macmini shell";
 
-        modules = [
-          ./macmini/home.nix
+        shellHook = ''
+          echo $'${zshrc}' > ~/.zshrc # Write the zsh config file
+          export LANG="en_US.UTF-8"
+          export IN_OUTER_SHELL=1
+        '';
+
+        packages = [
+          # System
+          pkgs.git
+          pkgs.wget
+          pkgs.curl
+          pkgs.jq
+
+          # Terminal Apps
+          pkgs.ncdu
+          pkgs.htop
+          pkgs.tmux
+          upkgs.helix
+          upkgs.gitui
+          pkgs.delta
+
+          #Apps
+          pkgs.anki-bin
         ];
-
-        extraSpecialArgs = { upkgs = upkgs; };
       };
     };
 }
