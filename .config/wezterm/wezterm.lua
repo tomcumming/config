5650e9397001892fccf23941f5f67618c54fa527
Tom Cumming <mail@tomcumming.co.uk>
2023-10-25 22:48:54 +0100

    Wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,11 @@
 local wezterm = require 'wezterm'
 
 local config = {
+  use_ime = true,
+  send_composed_key_when_left_alt_is_pressed = true,
   hide_tab_bar_if_only_one_tab = true,
-  font_size = 18
+  font_size = 20,
+  color_scheme = 'GitHub Dark'
 }
 
 return config
