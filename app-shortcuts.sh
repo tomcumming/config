2028100d5e559846c8b7d244c9456c1bb94c6cbb
Tom Cumming <mail@tomcumming.co.uk>
2023-10-25 23:34:01 +0100

    Alias rather than copy apps

---
+++ app-shortcuts.sh
@@ -0,0 +1,11 @@
+for appName in $(ls ~/.nix-profile/Applications)
+do
+  appFolder=~/Applications
+  app=~/.nix-profile/Applications/$appName
+  rm "$appFolder/$appName"
+  osascript \
+    -e "tell app \"Finder\"" \
+    -e "make new alias file at POSIX file \"$appFolder\" to POSIX file \"$app\"" \
+    -e  "set name of result to \"$appName\"" \
+    -e "end tell"
+done
--- copy-apps.sh
+++
@@ -1,7 +0,0 @@
-# Copy rather than link so it shows up in finder etc
-for appName in $(ls ~/.nix-profile/Applications)
-do
-  chmod -R +xw ~/Applications/$appName
-  rm -rf ~/Applications/$appName
-  cp -r -L ~/.nix-profile/Applications/$appName ~/Applications/$appName
-done
