cd9e5800ac34b4586ec68a4aa2f6cf4f3dfd6ac2
Tom Cumming <mail@tomcumming.co.uk>
2024-01-31 22:10:21 +0000

    new computer config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = {
   use_ime = true,
   send_composed_key_when_left_alt_is_pressed = true,
   hide_tab_bar_if_only_one_tab = true,
-  font_size = 20,
+  font_size = 15,
   color_scheme = 'GitHub Dark'
 }
 
