235bedc1646d7f427e9324aff3ba7b638b6aafb1
Tom Cumming <mail@tomcumming.co.uk>
2024-06-15 13:46:09 +0100

    Opt left/right for skip word

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,6 +8,13 @@ local config = {
   font_size = 15,
   color_scheme = 'GitHub Dark',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  },
 }
 
 return config
