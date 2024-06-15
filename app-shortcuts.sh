972b61e2c330f17328f0c04253d7f5b9b0c22547
Tom Cumming <mail@tomcumming.co.uk>
2024-06-15 13:45:40 +0100

    Reminder to force reindex for spotlight

--- app-shortcuts.sh
+++ app-shortcuts.sh
@@ -11,3 +11,8 @@ do
     -e  "set name of result to \"$appName\"" \
     -e "end tell"
 done
+
+echo '-----'
+echo 'Now goto: Settings -> Spotlight -> Privacy'
+echo 'Add add remove ~/Applications folder so it will reindex'
+# TODO there must be a better way...
